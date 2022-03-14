f60746336aad56e81f76a3a114a5d249c50328bc
Geethu20 <kollurigeethika@gmail.com>
2022-03-14 17:00:59 +0530

    Firt commit

---
+++ firstmavenproject/src/main/java/com/lti/example/Testcalculator.java
@@ -0,0 +1,10 @@
+package com.lti.example;
+
+public class Testcalculator {
+
+	public static void main(String arge[]) {
+		calculator c=new calculator();
+		int r=c.add(100,200);
+		System.out.println(r);
+	}
+}
